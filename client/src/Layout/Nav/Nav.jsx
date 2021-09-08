56fa884a49fa19d25d7092101ab65466923eb763
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-09-08 15:28:56 -0400

    created navigation bar

---
+++ client/src/Layout/Nav/Nav.jsx
@@ -0,0 +1,42 @@
+import "./Nav.css"
+import { Link } from "react-router-dom"
+
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className="nav-container">
+      <div className="nav-wrapper">
+
+        <div className="home-link-wrapper">
+          <Link to="/" className="home-link">
+            <h3 className="home-text">Portfolio</h3>
+          </Link>
+        </div>
+
+        <div className="name-wrapper">
+          <h2 className="name-text">Evan Alves</h2>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <div className="resume-link-wrapper">
+            <Link to="/resume" className="resume-link">
+              <h3>Resume</h3>
+            </Link>
+          </div>
+          <div className="about-link-wrapper">
+            <Link to="/about" className="about-link">
+              <h3>About</h3>
+            </Link>
+          </div>
+          <div className="contact-link-wrapper">
+            <Link to="/contact" className="contact-link">
+              <h3>Contact</h3>
+            </Link>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ client/src/Screens/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from "../../Layout/Nav/Nav"
+
+export default function Home() {
+  return (
+    <div className="home-wrapper">
+      <Nav />
+    </div>
+  )
+}
