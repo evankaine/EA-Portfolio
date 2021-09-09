a52adf1dc69cf59b484c97a0572564f92ef3a85a
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-09-09 15:36:10 -0400

    more previews added

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -14,28 +14,32 @@ export default function Home() {
         <div className="image-wrapper" id="projects">
           <a target="_blank" href="https://eathesyndicate.netlify.app/">
             <img className="syndicate-preview"
-              src="https://i.imgur.com/dZ0NCUX.png"
-              alt="syndicate preview">
+              src="https://i.imgur.com/a7SuIQd.png"
+              alt="Syndicate Preview">
             </img>
           </a>
         </div>
 
         <div className="image-wrapper">
-          <img className="syndicate-preview"
-            src="https://i.imgur.com/dZ0NCUX.png"
-            alt="syndicate preview" />
+          <a target="_blank" href="https://eaimgnation.netlify.app/">
+            <img className="syndicate-preview"
+              src="https://i.imgur.com/l35PCkg.png"
+              alt="imgNation Preview" />
+          </a>
+        </div>
+
+        <div className="image-wrapper">
+          <a target="_blank" href="https://eacrave.netlify.app/">
+            <img className="syndicate-preview"
+              src="https://i.imgur.com/I4LSTEC.png"
+              alt="Crave Preview" />
+          </a>
         </div>
 
         <div className="image-wrapper">
           <img className="syndicate-preview"
-            src="https://i.imgur.com/dZ0NCUX.png"
-            alt="syndicate preview" />
-        </div>
-
-        <div className="image-wrapper">
-          <img className="syndicate-preview"
-            src="https://i.imgur.com/dZ0NCUX.png"
-            alt="syndicate preview" />
+            src=""
+            alt="Wayfinder Preview" />
         </div>
       </div>
       <div className="scroll-to-top-wrapper">
